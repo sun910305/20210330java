ceec786e49ac20182f0f89d7d26d03bdcec27c92
sunwei <sun910305@gmail.com>
2021-04-13 22:52:48 +0800

    HorseRacing

---
+++ src/main/java/wei/racing/Racing.java
@@ -0,0 +1,14 @@
+package wei.racing;
+
+public class Racing {
+    public static void main(String[] args) {
+        Horse h1 = new Horse();
+        Horse h2 = new Horse();
+        HorseRunnable h3 = new HorseRunnable();
+        Thread thread = new Thread(h3);
+        thread.start();
+        h1.start();
+        h2.start();
+
+    }
+}
